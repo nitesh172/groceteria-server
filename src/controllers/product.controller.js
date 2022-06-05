123209610d95f1dcd850ba26a6caacccb19b35d7
Nitesh Kumar Baghel <niteshbaghel172@gmail.com>
2022-06-05 12:50:02 +0530

    Auth completed

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,6 @@
 const create = async (req, res) => {
   try {
-    res.status(201).send({url: req.file?.location});
+    res.status(201).json({url: req.file?.location});
   } catch (error) {
     console.log(error.message);
     res.status(500).send(error.message);
